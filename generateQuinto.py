5cba1e0876ede75bd438e48dea2cdeaf48de3a10
cestrada <cestrada@ikerlan.es>
2022-01-20 10:46:26 +0100

    👌 IMPROVE: Format

--- generateQuinto.py
+++ generateQuinto.py
@@ -1,15 +1,17 @@
 import random
 
-def shuffle(list, seed = None):
+
+def shuffle(list, seed=None):
 
     if seed is not None:
         random.seed(seed)
 
-    return random.sample(list,len(list))
+    return random.sample(list, len(list))
+
 
 def generateTable(numbers):
 
-    table = [[[0 for c in range(5) ] for r in range(3) ] for q in range(6) ]
+    table = [[[0 for c in range(5)] for r in range(3)] for q in range(6)]
 
     count = 0
 
@@ -18,11 +20,13 @@ def generateTable(numbers):
             for column in range(5):
                 table[quadrant][row][column] = numbers[count]
                 count += 1
-    
+
     return table
+
 
 def flattenQuadrant(quadrant):
     return [item for sublist in quadrant for item in sublist]
+
 
 def checkTable(table, listOfQuadrants, listOfRows):
 
@@ -37,20 +41,20 @@ def checkTable(table, listOfQuadrants, listOfRows):
                 if set(row).issubset(validRow):
                     print('Repeated row', row, validRow)
                     return False, listOfQuadrants, listOfRows
-    
 
     for quadrant in table:
         listOfQuadrants.append(quadrant)
         for row in quadrant:
             listOfRows.append(row)
-    
+
     return True, listOfQuadrants, listOfRows
+
 
 def generateValidTables(totalTables, initialSeed=0):
     print('Generation of Quinto tables started, please wait...')
     print('Press CTRL-C at any time to stop the generation and validation of Quinto tables if it takes too long.')
 
-    possibleNumbers = list(range(1,91))
+    possibleNumbers = list(range(1, 91))
 
     validTables = []
 
@@ -68,17 +72,18 @@ def generateValidTables(totalTables, initialSeed=0):
 
             generatedTable = generateTable(shuffledNumbers)
 
-            check, listOfQuadrants, listOfRows = checkTable(generatedTable, listOfQuadrants, listOfRows)
+            check, listOfQuadrants, listOfRows = checkTable(
+                generatedTable, listOfQuadrants, listOfRows)
 
             if check:
                 # validTables.append(shuffledNumbers)
                 validTables.append(generatedTable)
-    
+
     except KeyboardInterrupt:
         print('The eneration of Quinto tables has been interrupted.')
-    
+
     print('The total number of generated tables is:', len(validTables))
-    
+
     return validTables
 
 
